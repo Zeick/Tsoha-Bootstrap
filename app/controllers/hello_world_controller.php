ab027fce2f6bd3d6ecc02cd88b144eb85d2de576
Zeick <timo.j.karkkainen@helsinki.fi>
2017-11-11 01:52:28 +0200

    Tokan viikon palautus

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,8 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-        echo 'Tämä on kotisivu!';
+        //echo 'Tämä on kotisivu!';
+        View::make('home.html');
     }
 
     public static function sandbox(){
@@ -12,19 +13,56 @@
         View::make('helloworld.html');
         }
         
-    public static function game_list(){
-        View::make('suunnitelmat/game_list.html');
+    public static function poke_list(){
+        View::make('suunnitelmat/poke_list.html');
     }
     
-    public static function game_show(){
-        View::make('suunnitelmat/game_show.html');
+    public static function poke_show(){
+        View::make('suunnitelmat/poke_show.html');
     }
     
     public static function login(){
         View::make('suunnitelmat/login.html');
     }
     
-    public static function edit(){
-        View::make('suunnitelmat/edit.html');
+    public static function poke_edit(){
+        View::make('suunnitelmat/poke_edit.html');
     }
+    
+    public static function kouluttaja_list(){
+        View::make('suunnitelmat/kouluttaja_list.html');
+    }
+    
+    public static function kouluttaja_show(){
+        View::make('suunnitelmat/kouluttaja_show.html');
+    }
+
+    public static function kouluttaja_edit(){
+        View::make('suunnitelmat/kouluttaja_edit.html');
+    }
+
+    public static function liiga_list(){
+        View::make('suunnitelmat/liiga_list.html');
+    }
+
+    public static function liiga_show(){
+        View::make('suunnitelmat/liiga_show.html');
+    }
+
+    public static function liiga_edit(){
+        View::make('suunnitelmat/liiga_edit.html');
+    }
+    
+    public static function laji_list(){
+        View::make('suunnitelmat/laji_list.html');
+    }
+
+    public static function laji_show(){
+        View::make('suunnitelmat/laji_show.html');
+    }
+
+    public static function laji_edit(){
+        View::make('suunnitelmat/laji_edit.html');
+    }
+
   }
--- config/routes.php
+++ config/routes.php
@@ -8,15 +8,44 @@
     HelloWorldController::sandbox();
   });
   
-  $routes->get('/game', function(){
-    HelloWorldController::game_list();
+  $routes->get('/poke', function(){
+    HelloWorldController::poke_list();
   });
-  $routes->get('/game/1', function(){
-    HelloWorldController::game_show();
+  $routes->get('/poke/1', function(){
+    HelloWorldController::poke_show();
   });
   $routes->get('/login', function(){
     HelloWorldController::login();
   });
-  $routes->get('/edit', function(){
-    HelloWorldController::edit();
+  $routes->get('/poke_edit', function(){
+    HelloWorldController::poke_edit();
   });
+  $routes->get('/kouluttaja', function(){
+    HelloWorldController::kouluttaja_list();
+  });
+  $routes->get('/kouluttaja/1', function(){
+    HelloWorldController::kouluttaja_show();
+  });
+  $routes->get('/kouluttaja_edit', function(){
+    HelloWorldController::kouluttaja_edit();
+  });
+  $routes->get('/liiga', function(){
+  HelloWorldController::liiga_list();
+  });
+  $routes->get('/liiga/1', function(){
+  HelloWorldController::liiga_show();
+  });
+  $routes->get('/liiga_edit', function(){
+  HelloWorldController::liiga_edit();
+  });
+  $routes->get('/laji', function(){
+  HelloWorldController::laji_list();
+  });
+  $routes->get('/laji/1', function(){
+  HelloWorldController::laji_show();
+  });
+  $routes->get('/laji_edit', function(){
+  HelloWorldController::laji_edit();
+  });
+
+  
